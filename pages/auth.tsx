fedb61f7566d8b6687bb60e69aa4719e797fe63f
Dzaky Rifai <dzaky.pr.mulyono97@gmail.com>
2023-06-25 16:19:30 +0700

    test
    Signed-off-by: Dzaky Rifai <dzaky.pr.mulyono97@gmail.com>

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -53,19 +53,19 @@ const Auth = () => {
     }
   }, [email, password, router]);
 
-  const register = useCallback(async () => {
-    try {
-      await axios.post('/api/register', {
-        email,
-        name,
-        password,
-      });
+  // const register = useCallback(async () => {
+  //   try {
+  //     await axios.post('/api/register', {
+  //       email,
+  //       name,
+  //       password,
+  //     });
 
-      login();
-    } catch (error) {
-      console.log(error);
-    }
-  }, [email, name, password, login]);
+  //     login();
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }, [email, name, password, login]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -81,7 +81,7 @@ const Auth = () => {
               <Input id="email" type="email" label="Email address or phone number" value={email} onChange={(e: any) => setEmail(e.target.value)} />
               <Input type="password" id="password" label="Password" value={password} onChange={(e: any) => setPassword(e.target.value)} />
             </div>
-            <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+            <button onClick={login} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
               {variant === 'login' ? 'Login' : 'Sign up'}
             </button>
             {/* <div className="flex flex-row items-center gap-4 mt-8 justify-center">
