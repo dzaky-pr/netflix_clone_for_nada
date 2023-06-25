aac1d00ee8fef1bb9bb9dc31d7a8be870b2cef94
Dzaky Rifai <dzaky.pr.mulyono97@gmail.com>
2023-06-25 17:02:01 +0700

    fak
    Signed-off-by: Dzaky Rifai <dzaky.pr.mulyono97@gmail.com>

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -16,46 +16,41 @@ export const authOptions: AuthOptions = {
     //   clientId: process.env.GOOGLE_CLIENT_ID || '',
     //   clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     // }),
-    Credentials({
-      id: 'credentials',
-      name: 'Credentials',
-      credentials: {
-        email: {
-          label: 'Email',
-          type: 'text',
-        },
-        password: {
-          label: 'Password',
-          type: 'password',
-        },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error('Email and password required');
-        }
-
-        const user = await prismadb.user.findUnique({
-          where: {
-            email: credentials.email,
-          },
-        });
-
-        if (!user || !user.hashedPassword) {
-          throw new Error('Email does not exist');
-        }
-
-        const isCorrectPassword = await compare(credentials.password, user.hashedPassword);
-
-        if (!isCorrectPassword) {
-          throw new Error('Incorrect password');
-        }
-
-        return user;
-      },
-    }),
+    // Credentials({
+    //   id: 'credentials',
+    //   name: 'Credentials',
+    //   credentials: {
+    //     email: {
+    //       label: 'Email',
+    //       type: 'text',
+    //     },
+    //     password: {
+    //       label: 'Password',
+    //       type: 'password',
+    //     },
+    //   },
+    //   async authorize(credentials) {
+    //     if (!credentials?.email || !credentials?.password) {
+    //       throw new Error('Email and password required');
+    //     }
+    //     const user = await prismadb.user.findUnique({
+    //       where: {
+    //         email: credentials.email,
+    //       },
+    //     });
+    //     if (!user || !user.hashedPassword) {
+    //       throw new Error('Email does not exist');
+    //     }
+    //     const isCorrectPassword = await compare(credentials.password, user.hashedPassword);
+    //     if (!isCorrectPassword) {
+    //       throw new Error('Incorrect password');
+    //     }
+    //     return user;
+    //   },
+    // }),
   ],
   pages: {
-    signIn: '/auth',
+    signIn: '/profiles',
   },
   debug: process.env.NODE_ENV === 'development',
   adapter: PrismaAdapter(prismadb),
