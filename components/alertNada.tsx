3053a7f0f5ead30858fdb16d119c1161d0f4d655
Dzaky Rifai <dzaky.pr.mulyono97@gmail.com>
2023-06-26 23:32:41 +0700

    qr add

--- components/alertNada.tsx
+++ components/alertNada.tsx
@@ -9,14 +9,14 @@ const AlertNada = () => {
       imageHeight: 240,
       imageAlt: 'Special QR Code',
       title: 'Halo Cantik!',
-      text: `
+      html: `
         Selamat Ulang Tahun, Nada! <br><br> 
         Selamat ulang tahun yang ke-19! Hari ini adalah momen spesial yang menandai tahun baru dalam perjalanan hidupmu. <br><br> 
         Aku ingin mengucapkan selamat ulang tahun dengan penuh cinta dan harapan. <br><br> 
         Dalam 4 bulan terakhir, kita telah berbagi banyak kenangan indah bersama. Setiap saat yang kita lewati bersama-sama menjadi sebuah petualangan yang tak terlupakan. Melalui tawa, tangis, dan canda, kita tumbuh bersama dan saling mendukung satu sama lain. <br><br> 
         Semoga panjang umur, sehat selalu, makin pinter, cantik, sholeh, dan kurangin mangkelnya hehe.
       `,
-      confirmButtonText: 'Aamiin, aku janji',
+      confirmButtonText: 'Aamiin, aku janji hehe',
     });
   }, []);
 
