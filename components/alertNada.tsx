7a7f0b247fcd1acb71d82562fb95dada48a62984
Dzaky Rifai <dzaky.pr.mulyono97@gmail.com>
2023-07-19 23:41:00 +0700

    1month_update
    Signed-off-by: Dzaky Rifai <dzaky.pr.mulyono97@gmail.com>

--- components/alertNada.tsx
+++ components/alertNada.tsx
@@ -12,7 +12,7 @@ const AlertNada = () => {
       html: `
       Satu bulan telah berlalu sejak kita memulai perjalanan ini bersama-sama. Selama satu bulan ini, setiap momen bersamamu adalah kenangan indah yang tak terlupakan. Terima kasih telah membuat hidupku lebih berarti, berwarna, dan penuh cinta.
       <br/><br/>
-      Semoga kita dapat terus menjalin hubungan yang kuat dan saling mendukung satu sama lain. Mohon maaf aku masih jauh dari kata cukup dan sempurna. Aku bersyukur atas hadirnya kamu di duniaku, dan tak sabar untuk melihat apa yang masa depan miliki untuk kita berdua. Aamiin.
+      Semoga kita dapat terus menjalin hubungan yang kuat dan saling mendukung satu sama lain. Mohon maaf baru ini yang bisa aku kasih ke kamu. Sadar juga bahwa aku masih banyak kurang dan salahnya. Aku bersyukur atas hadirnya kamu di duniaku, dan tak sabar untuk melihat apa yang masa depan miliki untuk kita berdua. Aamiin.
       <br/><br/>
       Salam hangat, Mas Dzaky ❤️
       
